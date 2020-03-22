001b41b51aecbcf0f1eb12ac7d845eb169c35259
daspharion <daspharion@gmail.com>
2020-03-22 15:40:08 +0100

    landing page ready

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,10 +3,18 @@ import React, { Component } from 'react'
 import './styles/home.scss'
 
 export class Home extends Component<{}, {}> {
+  onLogin () {
+    alert('lol?')
+  }
+
   render () {
     return (
       <main className='home'>
-        <h1>This is the Home page</h1>
+        <div className='home-welcome'>
+          <span>A K A D E M I K</span>
+          <span>w twojej komórce</span>
+          <input type='button' value='zaloguj się' onClick={this.onLogin}/>
+        </div>
       </main>
     )
   }
--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -30,11 +30,11 @@ export class Navigation extends Component<{}, { toggle: Boolean }> {
     return (
       <nav className={`${toggle ? 'on' : ''}`}>
         <div>
-          <img src='images/logo.svg' />
+          <img src='images/logo.png' />
           <ul onClick={() => this.onLink()}>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/pricing'>Pricing</Link></li>
-            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/pricing'>Kontakt</Link></li>
+            <li><Link to='/about'>O Nas</Link></li>
           </ul>
         </div>
         <div className={`nav-toogle${toggle ? ' on' : ''}`} onClick={() => this.onToggle()}>
