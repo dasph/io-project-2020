45f2420ea538c3fcebe7183b38b65b4a251a7648
daspharion <daspharion@gmail.com>
2020-03-24 00:32:02 +0100

    'typo'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'io-project',
     script: 'dist/ioproject.js',
-    cwd: '/srv/ioproject',
+    cwd: '/srv/io-project-2020',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -14,7 +14,7 @@ module.exports = {
     production : {
       ref: 'origin/master',
       repo: 'git+https://github.com/daspharion/io-project-2020.git',
-      path: '/srv/ioproject',
+      path: '/srv/io-project-2020',
       'post-deploy': 'npm i && pm2 reload ecosystem.config.js'
     }
   }
