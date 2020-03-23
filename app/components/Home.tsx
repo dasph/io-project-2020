1b3432227af71dbd0cd33fe9bbf207f17d4521d2
daspharion <daspharion@gmail.com>
2020-03-24 00:38:41 +0100

    tip

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -12,6 +12,7 @@ export class Home extends Component<{}, {}> {
       <main className='home'>
         <div className='home-welcome'>
           <span>A K A D E M I K</span>
+          <span>T E S T I N P R O D U C T I O N</span>
           <span>w twojej komórce</span>
           <input type='button' value='zaloguj się' onClick={this.onLogin}/>
         </div>
