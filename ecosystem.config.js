2a15a04d330dfc2d955763196dac0036a2606d3e
daspharion <daspharion@gmail.com>
2020-03-24 01:27:10 +0100

    tip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '256M',
-    env: { NODE_ENV: 'production' }
+    max_memory_restart: '256M'
   }]
 }
