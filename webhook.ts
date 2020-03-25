a5ee7d8698202743313e6879c07ec70e750b3f5f
daspharion <daspharion@gmail.com>
2020-03-25 14:30:04 +0100

    webhook verification

--- webhook.ts
+++ webhook.ts
@@ -4,11 +4,12 @@ import { exec } from 'child_process'
 import { IncomingMessage } from 'http'
 import * as bodyPaser from 'koa-bodyparser'
 import { request, RequestOptions } from 'https'
+import { createHmac, timingSafeEqual } from 'crypto'
 
 type SlackRequestOptions = RequestOptions & { hostname: 'hooks.slack.com', payload: string }
 type HastebinRequestOptions = RequestOptions & { hostname: 'hastebin.com', payload: string }
 
-const { SLACK_NOTIFIER } = process.env
+const { WEBHOOK_SECRET, SLACK_NOTIFIER } = process.env
 
 const run = promisify(exec)
 const message = (head: string, state: string, key: string) => `> Head \`${head}\`: deployment *${state}*\n> Logs can be found <https://hastebin.com/${key}|here>`
@@ -53,8 +54,7 @@ const upload = (log: string): Promise<string> => send(headers.hastebin(log)).the
 const notify = (text: string): Promise<string> => send(headers.slack(`{"text":"${text}"}`))
 
 const deploy = (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>): void => {
-  const { after } = JSON.parse(ctx.request.body.payload)
-  const head = after.slice(0, 8)
+  const head = ctx.request.body.after.slice(0, 8)
 
   run('npm run deploy')
     .then(({ stdout }) => upload(stdout)).then((key) => notify(message(head, 'succeeded', key)))
@@ -63,12 +63,20 @@ const deploy = (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultConte
   ctx.body = 'OK'
 }
 
-const verify = (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>, next: Koa.Next): void => {
-  next()
+const verify = (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>, next: Koa.Next): Promise<any> => {
+  const sig = ctx.header['x-hub-signature']
+  const payload = JSON.stringify(ctx.request.body)
+  const hmac = createHmac('sha1', WEBHOOK_SECRET || '')
+  const digest = Buffer.from(`sha1=${hmac.update(payload).digest('hex')}`, 'utf8')
+  const checksum = Buffer.from(sig, 'utf8')
+
+  return (checksum.length === digest.length && timingSafeEqual(digest, checksum)) ? next() : ctx.throw(401)
 }
 
 new Koa()
   .use((ctx, next) => ctx.header['x-github-event'] === 'push' ? next() : ctx.throw(400))
   .use(bodyPaser())
+  .use((ctx, next) => ctx.request.body.ref === 'refs/heads/master' ? next() : (ctx.status = 204))
+  .use(verify)
   .use(deploy)
   .listen(8080)
