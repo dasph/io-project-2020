7b3fd817f72d5b3906cbdc580fad7277b8df2d48
daspharion <daspharion@gmail.com>
2020-03-25 06:02:48 +0100

    webhook almost complete

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps : [{
     name: 'io-project',
     script: 'dist/ioproject.js',
-    cwd: '/srv/io-project-2020',
     instances: 1,
     autorestart: true,
     watch: false,
--- webhook.ts
+++ webhook.ts
@@ -1,13 +1,70 @@
-const { createServer } = require('http')
-const { exec } = require('child_process')
+import * as Koa from 'koa'
+import { promisify } from 'util'
+import { exec } from 'child_process'
+import { IncomingMessage } from 'http'
+import * as bodyPaser from 'koa-bodyparser'
+import { request, RequestOptions } from 'https'
 
-createServer((req, res) => {
-  if (req.headers['x-github-event'] === 'push') {
-    console.log('starting deployment')
-    exec('npm run deploy', (a, b ,c) => console.log(a, b, c, 'deployment ended'))
-    return res.end()
-  }
+type SlackRequestOptions = RequestOptions & { hostname: 'hooks.slack.com', payload: string }
+type HastebinRequestOptions = RequestOptions & { hostname: 'hastebin.com', payload: string }
 
-  res.statusCode = 400
-  res.end()
-}).listen(8080)
+const { SLACK_NOTIFIER } = process.env
+
+const run = promisify(exec)
+const message = (head: string, state: string, key: string) => `Head \`${head}\`: deployment *${state}*\nLogs can be found <https://hastebin.com/${key}|here>`
+
+const headers = {
+  slack: (payload: string): SlackRequestOptions => ({
+    method: 'POST',
+    hostname: 'hooks.slack.com',
+    path: `/services/${SLACK_NOTIFIER}`,
+    headers: { 'Content-Type': 'application/json', 'Content-Length': payload.length },
+    payload
+  }),
+  hastebin: (payload: string): HastebinRequestOptions => ({
+    method: 'POST',
+    hostname: 'hastebin.com',
+    path: '/documents',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': payload.length },
+    payload
+  })
+}
+
+function send (options: SlackRequestOptions): Promise<string>
+function send (options: HastebinRequestOptions): Promise<{ key: string }>
+function send (options: SlackRequestOptions | HastebinRequestOptions) {
+  return new Promise<IncomingMessage>((resolve, reject) => {
+    const { payload, ...headers } = options
+
+    const req = request(headers, resolve).on('error', reject)
+    req.write(payload)
+    req.end()
+  }).then((stream) => new Promise<{ json: boolean, data: string }>((resolve, reject) => {
+    const json = stream.headers['content-type'] === 'application/json'
+    const data: Array<Buffer> = []
+
+    stream.on('data', (chunk) => (data.push(chunk)))
+    stream.on('end', () => resolve({ json, data: String(Buffer.concat(data)) }))
+    stream.on('error', reject)
+  })).then(({ json, data }) => json ? JSON.parse(data) : data)
+}
+
+const upload = (log: string): Promise<string> => send(headers.hastebin(log)).then(({ key }) => key)
+const notify = (text: string): Promise<string> => send(headers.slack(`{"text":"${text}"}`))
+
+const deploy = (ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>): void => {
+  const head = ctx.request.body.payload.after.slice(-8)
+
+  run('npm run deploy')
+    .then(({ stderr, stdout }) => stderr ? Promise.reject(new Error(stderr)) : Promise.resolve(stdout))
+    .then(upload).then((key) => notify(message(head, 'succeeded', key)))
+    .catch((e: Error) => upload(e.message).then((key) => notify(message(head, 'failed', key))))
+
+  ctx.body = 'OK'
+}
+
+new Koa()
+  .use((ctx, next) => ctx.header['x-github-event'] === 'push' ? next() : ctx.throw(400))
+  .use(bodyPaser())
+  .use(deploy)
+  .listen(8080)
