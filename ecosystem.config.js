532478dd3b9aa5b014222aabe98bfc72b64b363a
daspharion <daspharion@gmail.com>
2020-03-25 06:05:43 +0100

    typo | pm2 webhook

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,5 +6,12 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '256M'
+  }, {
+    name: 'github-webhook',
+    script: 'dist/webhook.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '64M'
   }]
 }
