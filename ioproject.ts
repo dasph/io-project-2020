af6f1e62bbace0c3c79864bc49a6deb88e440c47
daspharion <daspharion@gmail.com>
2020-08-06 21:07:46 +0300

    port from env

--- ioproject.ts
+++ ioproject.ts
@@ -1,10 +1,10 @@
 import Web from './src/web'
 import { createSecureServer, createServer } from './src/servers'
 
-const { NODE_ENV } = process.env
+const { NODE_ENV, PORT } = process.env
 
 console.log('Ξ Launching @io-project-2020')
 
 NODE_ENV === 'production'
-  ? createSecureServer(Web).listen(443)
+  ? createSecureServer(Web).listen(PORT)
   : createServer(Web).listen(80)
